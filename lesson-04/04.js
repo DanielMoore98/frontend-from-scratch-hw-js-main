d270075af300098a6148df7f89ae9c72c3898ed2
DanielMoore98 <daniel.m@trabemco.net>
2025-06-16 22:20:19 +0200

    lesson-04 Q4 complete

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,12 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let double = ''
+    for (let i = 0; i < str.length; i++){
+        double += str[i] + str[i]
+    }
+    return double
+}
+console.log(doubleEachCharacter('hello'));
+
